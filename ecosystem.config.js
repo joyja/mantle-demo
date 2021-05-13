a330d2eaccca0489cc66ace71e20563b3fa15016
James A. Joy <joyja@jarautomation.io>
2021-05-13 18:50:49 +0000

    added CD stuff

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [
+    {
+      name: 'mantle-demo',
+      script: 'npm',
+      args: 'start',
+      env_production: {
+        NODE_ENV: 'production'
+      }
+    }
+  ],
+
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'mantle-demo.lxd',
+      ref: 'origin/master',
+      repo: 'git@gitlab.com:joyja/mantle-demo.git',
+      path: '/home/ubuntu/mantle-demo',
+      'post-deploy':
+        'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production'
+    },
+  }
+}
