fa0d134b44521441e68628c178178f1afc8f574d
James A. Joy <joyja@jarautomation.io>
2021-05-14 19:10:18 +0000

    added process graphic

--- pages/index.vue
+++ pages/index.vue
@@ -1,37 +1,39 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-card class="mt-5">
-      <v-list v-if="edgeNodes.length > 0">
-        <v-list-item
-          v-for="metric in edgeNodes[0].devices[0].metrics"
-          :key="metric.name"
-        >
-          <v-list-item-content>
-            <v-list-item-title
-              >{{ metric.name }}: {{ metric.value }}</v-list-item-title
-            >
-            <v-list-item-subtitle>{{ metric.datatype }}</v-list-item-subtitle>
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-card>
-  </v-row>
+  <v-container>
+    <v-row justify="center" align="center">
+      <jar-process />
+    </v-row>
+    <v-row justify="center" align="center">
+      <v-card class="mt-5">
+        <v-list v-if="edgeNodes.length > 0">
+          <v-list-item
+            v-for="metric in edgeNodes[0].devices[0].metrics"
+            :key="metric.name"
+          >
+            <v-list-item-content>
+              <v-list-item-title
+                >{{ metric.name }}: {{ metric.value }}</v-list-item-title
+              >
+              <v-list-item-subtitle>{{ metric.datatype }}</v-list-item-subtitle>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-import graphql from '~/graphql'
+import Process from '~/components/Process'
 
 export default {
+  components: {
+    jarProcess: Process,
+  },
   data() {
     return {
       edgeNodes: [],
     }
-  },
-  apollo: {
-    edgeNodes: {
-      query: graphql.query.edgeNodes,
-      pollInterval: 5000,
-    },
   },
 }
 </script>
