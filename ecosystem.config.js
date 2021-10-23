a28eb159cb906c2be5e5c109bcc1f2cf6a1b97f3
James A. Joy <joyja@jarautomation.io>
2021-10-23 05:00:28 +0000

    added start script and edited ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,24 +2,13 @@ module.exports = {
   apps: [
     {
       name: 'mantle-demo',
-      script: 'npm',
-      args: 'start',
+      script: './scripts/start.js',
+      env: {
+        NODE_ENV: 'development',
+      },
       env_production: {
         NODE_ENV: 'production',
-        NUXT_HOST: '0.0.0.0',
       },
     },
   ],
-
-  deploy: {
-    production: {
-      user: 'ubuntu',
-      host: 'mantle-demo.lxd',
-      ref: 'origin/master',
-      repo: 'git@gitlab.com:joyja/mantle-demo.git',
-      path: '/home/ubuntu/mantle-demo',
-      'post-deploy':
-        'npm install && npm run build && pm2 startOrRestart ecosystem.config.js --env production',
-    },
-  },
 }
